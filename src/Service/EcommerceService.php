c9cff242abaac61c6a6a99385c7667e8f6705897
radid youssef <radid.yoyo@gmail.com>
2021-04-06 12:08:47 +0100

    hello méthode show nom Cat

--- src/Service/EcommerceService.php
+++ src/Service/EcommerceService.php
@@ -3,13 +3,30 @@
 
 namespace App\Service;
 
+use App\Entity\Categorie;
+use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 class EcommerceService
 {
+  private  $entityManager;
 
-
-    public function __construct()
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        
+        $this->entityManager = $entityManager;
     }
+
+
+    public function hello($id)
+    {
+
+        $categorie = $this->entityManager->getRepository(Categorie::class)->find($id);
+
+        $xml=$categorie->getNom();
+
+        return $xml;
+    }
+
+
 
 }
