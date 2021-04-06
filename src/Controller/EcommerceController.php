7dd60545a3d8de34cf22e7662dae31401658ae4c
radid youssef <radid.yoyo@gmail.com>
2021-04-06 10:17:22 +0100

    l'ajout service wsdl

--- src/Controller/EcommerceController.php
+++ src/Controller/EcommerceController.php
@@ -1,7 +1,6 @@
 <?php
-
 namespace App\Controller;
-
+use App\Service\EcommerceService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,13 +8,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class EcommerceController extends AbstractController
 {
     /**
-     * @Route("/ecommerce", name="ecommerce")
+     * @Route("/soap")
      */
-    public function index(): Response
+    public function index(EcommerceService $ecoService)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/EcommerceController.php',
-        ]);
+        $soapServer = new \SoapServer('http://127.0.0.1:8000/ecommerce.wsdl');
+        $soapServer->setObject($ecoService);
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'text/xml; charset=ISO-8859-1');
+
+        ob_start();
+        $soapServer->handle();
+        $response->setContent(ob_get_clean());
+
+        return $response;
     }
 }
